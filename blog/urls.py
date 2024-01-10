7afa0cc6f52884e742d43d06b1d700fc69f14f48
daliyev <daliyevazizbekk@gmail.com>
2024-01-10 23:23:16 +0500

    index publish field for better ordering, custom manager, configure admin page

---
+++ blog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+from django.conf.urls.static import static
+from django.conf import settings
+app_name = 'blog'
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+    path('<int:id>/', views.post_detail, name='post_detail'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
