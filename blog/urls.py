7f102880431bd1a787fb1e9b592e0687de1e34e3
daliyev <daliyevazizbekk@gmail.com>
2024-01-12 22:01:23 +0500

    added sending email using google smtp and comments

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,9 @@ from django.conf import settings
 app_name = 'blog'
 
 urlpatterns = [
-    path('', views.PostListView.as_view(), name='post_list'),
+    path('', views.post_list, name='post_list'),
     # SEO-friendly
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
+    path('<int:post_id>/share/', views.post_share, name='post_share'),
+    path('<int:post_id>/comment/', views.post_comment, name='post_comment'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- config/settings.py
+++ config/settings.py
@@ -127,3 +127,11 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'daliyev5797@gmail.com'
+EMAIL_HOST_PASSWORD = 'ijkcgkjjtfuoblkl'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
