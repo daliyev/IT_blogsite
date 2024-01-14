b885a42ce9ecdaa55f5e67d07828195cccf9571d
daliyev <daliyevazizbekk@gmail.com>
2024-01-15 00:51:20 +0500

    sitemap and feed for site more efficiently

--- blog/urls.py
+++ blog/urls.py
@@ -2,13 +2,16 @@ from django.urls import path
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
+from .feeds import LastedPostFeed
 app_name = 'blog'
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('test/', views.tests, name='test'),
     path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'),
     # SEO-friendly
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
     path('<int:post_id>/share/', views.post_share, name='post_share'),
     path('<int:post_id>/comment/', views.post_comment, name='post_comment'),
+    path('feed/', LastedPostFeed(), name='post_feed')
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- config/settings.py
+++ config/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+SITE_ID = 1
 
 # Application definition
 
@@ -41,6 +42,8 @@ INSTALLED_APPS = [
     'taggit',
     # local
     'blog.apps.BlogConfig',
+    'django.contrib.sites',
+    'django.contrib.sitemaps'
 ]
 
 MIDDLEWARE = [
