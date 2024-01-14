6fea444817ac9a2d826dca01d52bb3ad56c5f47e
daliyev <daliyevazizbekk@gmail.com>
2024-01-14 13:32:44 +0500

    total posts count, latest and most commented posts added to sidebar

---
+++ blog/templatetags/blog_tags.py
@@ -0,0 +1,36 @@
+from django import template
+from django.db.models import Count
+
+from ..models import Post
+
+
+register = template.Library()
+
+
+@register.simple_tag
+def total_posts():
+    return Post.published.count()
+
+
+@register.inclusion_tag('blog/post/latest_posts.html')
+def show_latest_posts(count=5):
+    latest_posts = (
+        Post.published
+        .only('title', 'publish')
+        .order_by('-publish')[:count])
+    ctx = {
+        'latest_posts': latest_posts,
+    }
+    return ctx
+
+
+@register.simple_tag
+def get_most_commented_posts(count=5):
+    queryset = (
+        Post.published
+        .prefetch_related('comments')
+        .only('title')
+        .annotate(total_comments=Count('comments'))
+        .order_by('-total_comments')[:count]
+    )
+    return queryset
